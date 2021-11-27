3e32e9139a1eb1d5f1a20673a6bf4d2fb5315e86
Kelvin <kelvinjune2@chaka.ng>
2021-11-28 00:11:12 +0100

    Switched project to use css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,40 +1,39 @@
 import { useState } from 'react';
-import styled from 'styled-components';
-
 import Button from '../../UI/Button/Button';
+import styles from './CourseInput.module.css';
 
-// import styles from './CourseInput.module.css';
+// import styled from 'styled-components';
 
 // using styled component to encapsulate styles
-const FormControl = styled.div`
-  & {
-    margin: 0.5rem 0;
-  }
+// const FormControl = styled.div`
+//   & {
+//     margin: 0.5rem 0;
+//   }
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${({ theme }) => (theme.invalid ? 'red' : 'black')};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${({ theme }) => (theme.invalid ? 'red' : 'black')};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${({ theme }) => (theme.invalid ? 'red' : '#ccc')};
-    background: ${({ theme }) =>
-      theme.invalid ? 'rgb(255, 215, 215)' : 'transparent'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${({ theme }) => (theme.invalid ? 'red' : '#ccc')};
+//     background: ${({ theme }) =>
+//       theme.invalid ? 'rgb(255, 215, 215)' : 'transparent'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -61,14 +60,14 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl
-        theme={{
-          invalid: !isValid,
-        }}
+      <div
+        className={`${styles['form-control']} ${
+          !isValid ? styles.invalid : ''
+        }`}
       >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
 
       <Button type="submit">Add Goal</Button>
     </form>
