d97e052ac84cb2775e003228727cea9ad19dcc95
Kelvin <kelvinjune2@chaka.ng>
2021-11-27 23:43:17 +0100

    using styled-components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
